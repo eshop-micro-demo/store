10c1851b5d8fa115dfa60324ef8f62aa2d2ba772
Vijay Dharap <dharapvj@gmail.com>
2021-05-20 13:23:05 +0530

    Basic spring security application on top of Spring Data Rest based store application. No real entities yet.

--- src/main/java/com/kubewarrior/store/FooRepository.java
+++ src/main/java/com/kubewarrior/store/FooRepository.java
@@ -5,8 +5,14 @@ import java.util.List;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 // @RepositoryRestResource(collectionResourceRel = "people", path = "people")
+@CrossOrigin(origins = "http://localhost:3000", maxAge = 3600, allowCredentials = "true")
+// @CrossOrigin
+// @CrossOrigin(allowCredentials = "true", origins = {"*"}, 
+//     methods = {RequestMethod.PUT, RequestMethod.POST, RequestMethod.GET, RequestMethod.DELETE, RequestMethod.OPTIONS})
 public interface FooRepository extends PagingAndSortingRepository<Foo, Long> {
 
   List<Foo> findByName(@Param("name") String name);
