07891dc7945db984c5f921bd3bfda5d8394ac8a2
Vijay Dharap <dharapvj@gmail.com>
2021-05-27 12:08:41 +0530

    Code changes to read externalized properties and to make Cors work with externalized properties.

--- src/main/java/com/kubewarrior/store/RestConfig.java
+++ src/main/java/com/kubewarrior/store/RestConfig.java
@@ -2,6 +2,7 @@ package com.kubewarrior.store;
 
 import com.kubewarrior.store.domain.Product;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
@@ -10,9 +11,19 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 @Configuration
 public class RestConfig implements RepositoryRestConfigurer {
 
+    @Value("${client.url}")
+    private String allowed_origins;
+
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
         config.exposeIdsFor(Product.class);
         // System.out.println("id exposed for product?: "+config.isIdExposedFor(Product.class));
+        System.out.println("Allowed origins: "+allowed_origins);
+        cors.addMapping("/**")
+            .allowedOrigins(allowed_origins)
+            // .allowedMethods("PUT", "DELETE")
+            // .allowedHeaders("header1", "header2", "header3")
+            // .exposedHeaders("header1", "header2")
+            .allowCredentials(true).maxAge(3600);
     }
 }
